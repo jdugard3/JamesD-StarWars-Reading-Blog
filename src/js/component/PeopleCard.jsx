220121565508be023028197bedba1d6c0ba3c71c
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-05-24 18:03:20 -0400

    mostly styling

--- src/js/component/PeopleCard.jsx
+++ src/js/component/PeopleCard.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const PeopleCard = ({ name, uid, url }) => {
   return (
     <>
-      <div className="col-3">
-        <div className="card" style={{ width: "18rem" }}>
+      <div className="col-3 d-flex justify-content-evenly m-1">
+        <div className="card" style={{ width: "20rem" }}>
           <img
             src={`https://starwars-visualguide.com/assets/img/characters/${uid}.jpg`}
             className="card-img-top"
@@ -13,8 +13,20 @@ const PeopleCard = ({ name, uid, url }) => {
           />
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
+            <p className="">Gender:</p>
+            <p className="">Hair-Color:</p>
+            <p className="">Eye-Color:</p>
+
             <Link to={`/people/${uid}`}>
-              <button className="btn btn-primary">Learn more</button>
+              <button className="btn btn-danger">Learn more</button>
+            </Link>
+            
+            <Link>
+              <button className="btn btn btn-outline-warning m-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                </svg>
+              </button>
             </Link>
           </div>
         </div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,53 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star Wars</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
-				</Link>
-			</div>
-		</nav>
+		<>
+			<nav className="navbar navbar-expand-lg bg-dark">
+				<div className="container-fluid">
+					<Link to="/">
+		 				<img className="img border-0" src="https://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG11.png" style={{width: "100px"}}></img>
+					</Link>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item dropdown">
+								<a className="dropdown-toggle btn btn-outline-warning" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										Favorites
+								</a>
+								<ul className="dropdown-menu">
+									<li><a className="dropdown-item" href="#">Action</a></li>
+									<li><a className="dropdown-item" href="#">Another action</a></li>
+									<li><hr className="dropdown-divider" /></li>
+									<li><a className="dropdown-item" href="#">Something else here</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
 	);
 };
+
+
+
+
+
+
+		
+		{/* // <nav className="navbar navbar-light bg-dark mb-3">
+		// 	<Link to="/">
+		// 		<span className="navbar-brand mb-0 h1">Star Wars</span>
+		// 	</Link>
+		// 	<div className="ml-auto dropdown">
+		// 		<Link to="/demo">
+		// 			<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="#favoritesDropDown" aria-expanded="false">Favorites</button>
+		// 		</Link>
+		// 		<ul className="dropdown-menu dropdown-menu-dark">
+		// 			<li><a className="dropdown-item active" href="#">Action</a></li>
+		// 			<li><a className="dropdown-item" href="#">Another action</a></li>
+		// 			<li><a className="dropdown-item" href="#">Something else here</a></li>
+		// 			<li><hr className="dropdown-divider" /></li>
+		// 			<li><a className="dropdown-item" href="#">Separated link</a></li>
+		// 		</ul>
+		// 	</div>
+		// </nav> */}
